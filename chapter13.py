5f6774f9e859210d650298774f5bdf468f58fb6c
anatta <qj135@qq.com>
2016-06-06 23:39:11 +0800

    test

--- chapter13.py
+++ chapter13.py
@@ -5,5 +5,30 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 13-3
+class float2money:
+    def __init__(self, money):
+        self.mn = money
+
+    def dollarize(self):
+        pass
+
+
+
+
+# page 405
 if __name__ == '__main__':
     pass
